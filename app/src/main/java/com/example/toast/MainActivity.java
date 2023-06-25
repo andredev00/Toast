06c669cd8e7cd846d4d3089ad833306a9a453bc4
André Ferreira <andreferreira6578@gmail.com>
2023-06-25 15:27:42 +0100

    Finished simple application with increase and decrease count numbers

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    private Toast toast;
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @SuppressLint({"ResourceType", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        this.toast = Toast.makeText(this, R.string.hello_toast, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @SuppressLint("ResourceType")
+    public void countToast(View view) {
+        if (mShowCount != null) {
+            mCount++;
+            if(mCount > 100){
+                this.toast = Toast.makeText(this, R.string.max_limit_count, Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void decreaseToast(View view) {
+        if (mShowCount != null) {
+            mCount--;
+            if(mCount < 0){
+                this.toast = Toast.makeText(this, R.string.min_limit_count, Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
